3fb48070c13fd5d61498dfe7f56d2febf2974765
Seb Hirsch <50666797+cyberhirsch@users.noreply.github.com>
2026-04-19 12:19:00 +0200

    chore: setup custom domain gulerhirsch.com and fix asset paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://cyberhirsch.github.io',
-    base: '/Website-GulerHirsch',
+    site: 'https://gulerhirsch.com',
+    base: '/',
     integrations: [sitemap()],
 });
